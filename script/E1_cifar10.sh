efdec00ed79fef742ef4fb98e5e6e038512ca460
yuchen <yc.yang413@gmail.com>
2023-06-25 22:21:38 -0400

    Update scripts for all epsilon

--- script/E1_cifar10.sh
+++ script/E1_cifar10.sh
@@ -1,9 +1,9 @@
 start=$(date +%s)
 cd .. &&
 
-python FedAverage.py --data='cifar10' --nclient=100 --nclass=10 --ncpc=2 --model='resnet18_IN' --mode='LDP' --round=60 --epsilon=2 --sr=1 --lr=5e-2 --flr=1e-1 --physical_bs=3 --E=1
-python FedAverage.py --data='cifar10' --nclient=100 --nclass=10 --ncpc=2 --model='resnet18_IN' --mode='LDP' --round=80 --epsilon=4 --sr=1 --lr=5e-2 --flr=1e-1 --physical_bs=3 --E=1
-python FedAverage.py --data='cifar10' --nclient=100 --nclass=10 --ncpc=2 --model='resnet18_IN' --mode='LDP' --round=100 --epsilon=6 --sr=1 --lr=5e-2 --flr=1e-1 --physical_bs=3 --E=1
+python FedAverage.py --data='cifar10' --nclient=100 --nclass=10 --ncpc=2 --model='resnet18_IN' --mode='LDP' --round=80 --epsilon=2 --sr=1 --lr=5e-2 --flr=1e-1 --physical_bs=3 --E=1
+python FedAverage.py --data='cifar10' --nclient=100 --nclass=10 --ncpc=2 --model='resnet18_IN' --mode='LDP' --round=100 --epsilon=4 --sr=1 --lr=5e-2 --flr=1e-1 --physical_bs=3 --E=1
+python FedAverage.py --data='cifar10' --nclient=100 --nclass=10 --ncpc=2 --model='resnet18_IN' --mode='LDP' --round=130 --epsilon=6 --sr=1 --lr=5e-2 --flr=1e-1 --physical_bs=3 --E=1
 python FedAverage.py --data='cifar10' --nclient=100 --nclass=10 --ncpc=2 --model='resnet18_IN' --mode='LDP' --round=150 --epsilon=8 --sr=1 --lr=5e-2 --flr=1e-1 --physical_bs=3 --E=1
 
 python FedAverage.py --data='cifar10' --nclient=100 --nclass=10 --ncpc=2 --model='resnet18_IN' --mode='CDP' --round=30 --epsilon=2 --sr=1 --lr=5e-2 --flr=1e-1 --physical_bs=8 --E=1
